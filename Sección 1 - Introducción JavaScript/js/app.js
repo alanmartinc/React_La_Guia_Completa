3503b3f802ad09354fae81fc91355b77fdf027c2
alanmartinc <alancabot.ac@gmail.com>
2021-01-31 05:05:13 -0300

    Se agrega ejercicio Scope en JavaScript

--- Sección 1 - Introducción JavaScript/js/app.js
+++ Sección 1 - Introducción JavaScript/js/app.js
@@ -1,4 +1,30 @@
-const aprendiendo1 = 'JavaScript';
-let aprendiendo2 = "React";
-aprendiendo2 = true;
-console.log(aprendiendo1, aprendiendo2);
+// VAR
+var musica1 = 'Rock';
+
+if(musica1) {
+    var musica1 = 'Grunge';
+    console.log('Dentro del IF: ', musica1);
+}
+console.log('Fuera del IF: ', musica1);
+
+console.log('<br>');
+
+// LET
+let musica2 = 'Rock';
+
+if(musica2) {
+    let musica2 = 'Grunge';
+    console.log('Dentro del IF: ', musica2);
+}
+console.log('Fuera del IF: ', musica2);
+
+console.log('<br>');
+
+// CONST
+const musica3 = 'Rock';
+
+if(musica3) {
+    const musica3 = 'Grunge';
+    console.log('Dentro del IF: ', musica3);
+}
+console.log('Fuera del IF: ', musica3);
