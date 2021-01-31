7fd52bdb02f5d9de36d39e9883b71cea78462a6f
alanmartinc <alancabot.ac@gmail.com>
2021-01-31 06:25:51 -0300

    Se agrega ejercicio objetos en JavaScript - object constructor

--- Sección 1 - Introducción JavaScript/js/app.js
+++ Sección 1 - Introducción JavaScript/js/app.js
@@ -1,9 +1,17 @@
-// Object literal
-const persona = {
-    nombre: 'Alan',
-    edad: 24
+// Object constructor
+function Tarea(nombre, urgencia) {
+    this.nombre = nombre;
+    this.urgencia = urgencia;
 }
 
-console.log(persona);
-console.log(persona.nombre);
-console.log(persona['edad']);
+// Crear una nueva tarea
+const tarea1 = new Tarea('Aprender JavaScript y React', 'Urgente');
+const tarea2 = new Tarea('Pasear al Perro', 'Media');
+const tarea3 = new Tarea('Conocer a mis suegros', 'Baja');
+
+console.log(tarea1);
+console.log(tarea2);
+console.log(tarea3);
+
+console.log(tarea1.nombre);
+console.log(tarea1.urgencia);
