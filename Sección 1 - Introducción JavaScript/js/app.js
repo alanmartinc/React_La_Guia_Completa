d154c09c44bb5a59d3d0b7c61e02193d2b2ffa23
alanmartinc <alancabot.ac@gmail.com>
2021-01-31 06:02:01 -0300

    Se agrega ejercicio funciones con parametros

--- Sección 1 - Introducción JavaScript/js/app.js
+++ Sección 1 - Introducción JavaScript/js/app.js
@@ -1,11 +1,14 @@
 // Function declaration
-saludar('Alan');
-function saludar(nombre){
-    console.log('Bienvenido ' + nombre);
+function actividad1(nombre, actividad){
+    console.log(`La persona ${nombre}, esta realizando la actividad ${actividad}`);
 }
 
+actividad1('Alan', 'Aprender JavaScript');
+actividad1();
+
 // Function expression
-const cliente = function(nombreCliente){
-    console.log('Mostrando datos del cliente: ' + nombreCliente);
+const actividad2 = function(nombre='Lucas', actividad='Aprender Python'){
+    console.log(`La persona ${nombre}, esta realizando la actividad ${actividad}`);
 }
-cliente('Alan');
+
+actividad2();
