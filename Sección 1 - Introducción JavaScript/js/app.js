76da1d0cd401f7e581c2aa16565f423759ebe70b
alanmartinc <alancabot.ac@gmail.com>
2021-01-31 06:39:05 -0300

    Se agrega ejercicio prototypes

--- Sección 1 - Introducción JavaScript/js/app.js
+++ Sección 1 - Introducción JavaScript/js/app.js
@@ -4,14 +4,12 @@ function Tarea(nombre, urgencia) {
     this.urgencia = urgencia;
 }
 
+// Agregar un prototype a tarea:
+Tarea.prototype.mostrarInformacionTarea = function(){
+    return `La tarea ${this.nombre} tiene una prioridad de ${this.urgencia}`;
+}
+
 // Crear una nueva tarea
 const tarea1 = new Tarea('Aprender JavaScript y React', 'Urgente');
-const tarea2 = new Tarea('Pasear al Perro', 'Media');
-const tarea3 = new Tarea('Conocer a mis suegros', 'Baja');
-
 console.log(tarea1);
-console.log(tarea2);
-console.log(tarea3);
-
-console.log(tarea1.nombre);
-console.log(tarea1.urgencia);
+console.log(tarea1.mostrarInformacionTarea());
