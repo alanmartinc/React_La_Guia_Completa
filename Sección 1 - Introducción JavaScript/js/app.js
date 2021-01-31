4d7eef19adceef64804e4d26737deadc81389040
alanmartinc <alancabot.ac@gmail.com>
2021-01-31 06:18:20 -0300

    Se agrega ejercicio objetos en JavaScript - object literal

--- Sección 1 - Introducción JavaScript/js/app.js
+++ Sección 1 - Introducción JavaScript/js/app.js
@@ -1,16 +1,9 @@
-let viajando1 = function(destino){
-    return `Viajando a la ciudad de: ${destino}`;
+// Object literal
+const persona = {
+    nombre: 'Alan',
+    edad: 24
 }
 
-let viaje1;
-viaje1 = viajando1('Buenos Aires');
-
-console.log(viaje1);
-
-// Arrow functions simplificado
-let viajando2 = destino => `Viajando a la ciudad de: ${destino}`;
-
-let viaje2;
-viaje2 = viajando2('Londres');
-
-console.log(viaje2);
+console.log(persona);
+console.log(persona.nombre);
+console.log(persona['edad']);
