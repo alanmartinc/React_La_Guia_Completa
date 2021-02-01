37c678ed8ccc3b7248e5ccb3528f2d28d5c45438
alanmartinc <alancabot.ac@gmail.com>
2021-02-01 06:03:18 -0300

    Se agrega ejercicio programación orientada a objetos - Clases

--- Sección 1 - Introducción JavaScript/js/app.js
+++ Sección 1 - Introducción JavaScript/js/app.js
@@ -1,46 +1,19 @@
-// Promises con Ajax
-const descargarUsuarios = cantidad => new Promise((resolve, reject) => {
-    const api = `https://randomuser.me/api/?results=${cantidad}&nat=us`;
-
-    // Llamado a Ajax
-    const xhr = new XMLHttpRequest();
-
-    // Abrir la conexión
-    xhr.open('GET', api, true);
-
-    // On load
-    xhr.onload = () => {
-        if(xhr.status === 200) {
-            resolve(JSON.parse(xhr.responseText).results);
-        } else {
-            reject(Error(xhr.statusText));
-        }
+// Escribir Clases
+class Tarea {
+    constructor(nombre, prioridad) {
+        this.nombre = nombre;
+        this.prioridad = prioridad;
     }
-
-    // Opcional on error
-    xhr.onerror = (error) => reject(error);
-
-    // Send
-    xhr.send();
-});
-
-// Me muestra 20 usuarios
-descargarUsuarios(20).then(
-    miembros => imprimirHTML(miembros),
-    error => console.error(new Error('Hubo un error' + error))
-)
-
-// Me los muestra de forma individual
-function imprimirHTML(usuarios){
-    let html = '';
-    usuarios.forEach(usuario => {
-        html += `
-            <li>Nombre: ${usuario.name.first} ${usuario.name.last}</li>
-            <li>Pais: ${usuario.nat}</li>
-            <li>Imagen: <img src="${usuario.picture.medium}"></li>
-        `;
-    });
-
-    const contenedorApp = document.querySelector('#app');
-    contenedorApp.innerHTML = html;
+    mostrar() {
+        return(`${this.nombre}, tiene una prioridad de ${this.prioridad}`);
+    }
 }
+
+// Crear los objetos
+let tarea1 = new Tarea('Aprender JavaScript', 'Alta');
+let tarea2 = new Tarea('Pasear al perro', 'Media');
+let tarea3 = new Tarea('Conocer a mis suegros', 'Baja');
+
+console.log(tarea1.mostrar());
+console.log(tarea2.mostrar());
+console.log(tarea3.mostrar());
