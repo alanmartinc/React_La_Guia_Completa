874b79615d1f0f8505de667d14cd186e655746c1
alanmartinc <alancabot.ac@gmail.com>
2021-02-01 16:05:17 -0300

    Se agrega ejercicio primeros pasos con el State de React

--- Sección 2 - Introducción y Básicos de React/react-fundamentos/src/App.js
+++ Sección 2 - Introducción y Básicos de React/react-fundamentos/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Section from './components/Section'
 
 function App() {
+  // Crear listado de productos
+  // En los corchetes se pasan o extraen dos valores: 
+  // El primero es el state.
+  // El segundo es una funcion que te ayuda a re-escribir el state.
+  const [productos, guardarProductos] = useState([
+    // Valores iniciales
+    {id: 1, nombre: 'Camisa ReactJS', precio: 50},
+    {id: 2, nombre: 'Camisa VueJS', precio: 40},
+    {id: 3, nombre: 'Camisa NodeJS', precio: 30},
+    {id: 4, nombre: 'Camisa AngularJS', precio: 20},
+  ]);
+
   // Obtener la fecha
   const fecha = new Date().getFullYear();
   return (
