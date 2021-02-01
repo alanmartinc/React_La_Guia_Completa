bf93004d2c6bc1bfe2a5957f4b58754255844097
alanmartinc <alancabot.ac@gmail.com>
2021-01-31 21:14:05 -0300

    Se agrega ejercicio funciones en un objeto

--- Sección 1 - Introducción JavaScript/js/app.js
+++ Sección 1 - Introducción JavaScript/js/app.js
@@ -1,22 +1,10 @@
-// Object literal enhancement
-const banda = 'Metallica';
-const genero = 'Heavy Metal';
-const canciones = ['Master Of Puppets', 'Seek & Destroy', 'Enter Sandman'];
-
-// Versión anterior
-const metallica1 = {
-    banda: banda,
-    genero: genero,
-    canciones: canciones
+// Métodos o funciones en un objeto
+const persona = {
+    nombre: 'Alan',
+    edad: 24,
+    mostrarInformacion(){
+        console.log(`${this.nombre} tiene ${this.edad} años.`);
+    }
 }
 
-console.log(metallica1);
-
-// Versión nueva
-const metallica2 = {banda, genero, canciones};
-console.log(metallica2);
-
-/* 
-Object destructuring: Extraer valores de un objeto.
-Object literal enhancement: Para ponerlos juntos en un solo objeto.
-*/
+persona.mostrarInformacion();
