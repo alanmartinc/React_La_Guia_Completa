5bcab15fecd5b83cbc3c13f7ef0b116e02e810b1
alanmartinc <alancabot.ac@gmail.com>
2021-02-01 06:24:44 -0300

    Se agrega ejercicio programación orientada a objetos - Clases - heredando de una Clase

--- Sección 1 - Introducción JavaScript/js/app.js
+++ Sección 1 - Introducción JavaScript/js/app.js
@@ -9,6 +9,22 @@ class Tarea {
     }
 }
 
+// Heredando Clases
+class ComprasPendientes extends Tarea {
+    constructor(nombre, prioridad, cantidad) {
+        // Hacemos referencia al constructor Padre
+        super(nombre, prioridad);
+
+        this.cantidad = cantidad;
+    }   
+    mostrar(){
+        return(`${this.nombre}, tiene una prioridad de ${this.prioridad} y la cantidad de ${this.cantidad}`);
+    }
+    Descripcion(){
+        return `Soy un ${this.nombre}`;
+    }
+}
+
 // Crear los objetos
 let tarea1 = new Tarea('Aprender JavaScript', 'Alta');
 let tarea2 = new Tarea('Pasear al perro', 'Media');
@@ -17,3 +33,11 @@ let tarea3 = new Tarea('Conocer a mis suegros', 'Baja');
 console.log(tarea1.mostrar());
 console.log(tarea2.mostrar());
 console.log(tarea3.mostrar());
+
+// Compras
+let compra1 = new ComprasPendientes('Jabon', 'Urgente', 3);
+
+// Ademas de heredar clases, tambien podemos heredar metodos
+console.log(compra1.mostrar());
+
+console.log(compra1.Descripcion());
