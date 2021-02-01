81b6f5a027f3350b7633db08f0862c460d047ab4
alanmartinc <alancabot.ac@gmail.com>
2021-02-01 13:48:34 -0300

    Se agrega ejercicio básicos de JSX

--- Sección 2 - Introducción y Básicos de React/react-fundamentos/src/components/Header.jsx
+++ Sección 2 - Introducción y Básicos de React/react-fundamentos/src/components/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
 function Header(){
+    // Lógica JavaScript
+    const edad = 18;
+    let mensaje;
+
+    if(edad >= 18){
+        mensaje = 'Eres mayor de edad';
+    } else {
+        mensaje = 'Eres menor de edad';
+    }
 
     // Lo que este en el return es lo que se ve en pantalla
     return(
-        <h1>Desde el Header</h1>
+        <h1 className="titulo">{mensaje}</h1>
     )
 }
 
