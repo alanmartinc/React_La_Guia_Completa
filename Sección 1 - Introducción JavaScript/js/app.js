6afc16e1f211da1abbafa083c332d4af0041ecc9
alanmartinc <alancabot.ac@gmail.com>
2021-02-01 06:45:09 -0300

    Se agrega ejercicio Módulos en ES6

--- Sección 1 - Introducción JavaScript/js/app.js
+++ Sección 1 - Introducción JavaScript/js/app.js
@@ -1,43 +1,14 @@
-// Escribir Clases
-class Tarea {
-    constructor(nombre, prioridad) {
-        this.nombre = nombre;
-        this.prioridad = prioridad;
-    }
-    mostrar() {
-        return(`${this.nombre}, tiene una prioridad de ${this.prioridad}`);
-    }
-}
+// Forma número uno
+import {nombreTarea} from './tareas.js';
+console.log(nombreTarea);
 
-// Heredando Clases
-class ComprasPendientes extends Tarea {
-    constructor(nombre, prioridad, cantidad) {
-        // Hacemos referencia al constructor Padre
-        super(nombre, prioridad);
+// Forma número dos
+import urgencia from './tareas.js';
+console.log(urgencia);
 
-        this.cantidad = cantidad;
-    }   
-    mostrar(){
-        return(`${this.nombre}, tiene una prioridad de ${this.prioridad} y la cantidad de ${this.cantidad}`);
-    }
-    Descripcion(){
-        return `Soy un ${this.nombre}`;
-    }
-}
-
-// Crear los objetos
-let tarea1 = new Tarea('Aprender JavaScript', 'Alta');
-let tarea2 = new Tarea('Pasear al perro', 'Media');
-let tarea3 = new Tarea('Conocer a mis suegros', 'Baja');
-
-console.log(tarea1.mostrar());
-console.log(tarea2.mostrar());
-console.log(tarea3.mostrar());
-
-// Compras
-let compra1 = new ComprasPendientes('Jabon', 'Urgente', 3);
-
-// Ademas de heredar clases, tambien podemos heredar metodos
-console.log(compra1.mostrar());
-
-console.log(compra1.Descripcion());
+/* 
+Módulos en ES6:
+1) Exportar script
+2) Importar script y con el from le decimos el nombre del archivo
+3) En el index.html en el script, agregar type="module"
+*/
---
+++ Sección 1 - Introducción JavaScript/js/tareas.js
@@ -0,0 +1,6 @@
+// Forma número uno
+export const nombreTarea = 'Pasear al perro';
+
+// Forma número dos
+const urgencia = 'Media';
+export default urgencia;
