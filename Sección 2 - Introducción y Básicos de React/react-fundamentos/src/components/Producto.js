88609d9489d6b0f8dbcb724d1b34489faa7e0d3d
alanmartinc <alancabot.ac@gmail.com>
2021-02-01 20:29:22 -0300

    Se agrega ejercicio eventos en React

--- Sección 2 - Introducción y Básicos de React/react-fundamentos/src/components/Producto.js
+++ Sección 2 - Introducción y Básicos de React/react-fundamentos/src/components/Producto.js
@@ -4,13 +4,19 @@ const Producto = ({producto}) => {
     // Extraer los valores
     const {nombre, precio, id} = producto;
 
+    // Agregar producto al carrito
+    const seleccionarProducto = (id) => {
+        console.log('Comprando...', id);
+    }
+
     return (
         <div>
             <h2>{nombre}</h2>
             <h3>${precio}</h3>
             <h4>{id}</h4>
+            <button type="button" id="comprar" onClick={() => seleccionarProducto(id)}>Comprar</button>
         </div>
     );
 }
- 
+
 export default Producto;
