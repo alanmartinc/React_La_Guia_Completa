cd72f0bf01851b8b2d87015fe09ffa9127948680
alanmartinc <alancabot.ac@gmail.com>
2021-02-01 13:18:33 -0300

    Se agrega ejercicio creando nuestro primer componente

--- Sección 2 - Introducción y Básicos de React/react-fundamentos/src/App.js
+++ Sección 2 - Introducción y Básicos de React/react-fundamentos/src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Mundo!
-        </p>
-      </header>
+      <Header/>
     </div>
   );
 }
---
+++ Sección 2 - Introducción y Básicos de React/react-fundamentos/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(){
+
+    // Lo que este en el return es lo que se ve en pantalla
+    return(
+        <h1>Desde el Header</h1>
+    )
+}
+
+export default Header;
