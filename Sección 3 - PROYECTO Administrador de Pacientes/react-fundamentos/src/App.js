1a78ad31965287db3756de4927086a85091039df
alanmartinc <alancabot.ac@gmail.com>
2021-02-20 02:31:16 -0300

    Se agrega ejercicios creando el componente de formulario

--- Sección 3 - PROYECTO Administrador de Pacientes/react-fundamentos/src/App.js
+++ Sección 3 - PROYECTO Administrador de Pacientes/react-fundamentos/src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import Formulario from './components/Formulario'
 
 function App() {
   return (
+    <Fragment>
       <h1>Administrador de Pacientes</h1>
+      <div className="container">
+
+        <div className="row">
+          <div className="one-half column">
+            <Formulario/>
+          </div>
+          <div className="one-half column">
+            2
+          </div>
+        </div>
+
+      </div>
+    </Fragment>
   );
 }
 
---
+++ Sección 3 - PROYECTO Administrador de Pacientes/react-fundamentos/src/components/Formulario.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Formulario = () => {
+    return(
+        <h1>Desde Formulario</h1>
+    );
+}
+
+export default Formulario;
