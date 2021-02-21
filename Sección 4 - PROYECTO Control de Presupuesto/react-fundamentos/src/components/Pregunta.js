692291ca68ee4322ebe90b6768b6a08c0b112fcc
alanmartinc <alancabot.ac@gmail.com>
2021-02-21 04:03:41 -0300

    Se agrega ejercicios mostrando mensajes de error

--- Sección 4 - PROYECTO Control de Presupuesto/react-fundamentos/src/components/Pregunta.js
+++ Sección 4 - PROYECTO Control de Presupuesto/react-fundamentos/src/components/Pregunta.js
@@ -1,4 +1,5 @@
 import React, {Fragment, useState} from 'react';
+import Error from './Error'
 
 const Pregunta = () => {
     // Definir el State
@@ -27,6 +28,8 @@ const Pregunta = () => {
     return(
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
+
+            { error ? <Error mensaje="El Presupuesto es Incorrecto"/>  : null }
 
             <form onSubmit={agregarPresupuesto}>
                 <input
