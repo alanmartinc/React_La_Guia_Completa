0db2ce1d4c330ae7e218aad4012f46daf5f8912c
alanmartinc <alancabot.ac@gmail.com>
2021-02-22 18:16:49 -0300

    Se agrega ejercicios validando los gastos

--- Sección 4 - PROYECTO Control de Presupuesto/react-fundamentos/src/components/Formulario.js
+++ Sección 4 - PROYECTO Control de Presupuesto/react-fundamentos/src/components/Formulario.js
@@ -1,17 +1,28 @@
 import React, {useState} from 'react';
+import Error from './Error'
 
 const Formulario = () => {
     const [nombre, guardarNombre] = useState('')
     const [cantidad, guardarCantidad] = useState(0)
+    const [error, guardarError] = useState(false)
 
     // Cuando un usuario agrega un gasto
     const agregarGasto = e => {
         e.preventDefault()
+
+        // Validar
+        if(cantidad < 1 || isNaN(cantidad || nombre.trim() === '')) {
+            guardarError(true)
+            return
+        }
+        guardarError(false)
     }
 
     return(
         <form onSubmit={agregarGasto}>
             <h2>Agrega tus gastos aquí</h2>
+
+            { error ? <Error mensaje="Ambos campos son obligatorios o Presupuesto Incorrecto"/> : null }
 
             <div className="campo">
                 <label>Nombre Gasto</label>
