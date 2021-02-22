00628abf160a08f96d5c8dbcfb94d7d654cf08c6
alanmartinc <alancabot.ac@gmail.com>
2021-02-22 17:48:06 -0300

    Se agrega ejercicios creando el State para los gastos

--- Sección 4 - PROYECTO Control de Presupuesto/react-fundamentos/src/components/Formulario.js
+++ Sección 4 - PROYECTO Control de Presupuesto/react-fundamentos/src/components/Formulario.js
@@ -1,18 +1,26 @@
 import React, {useState} from 'react';
 
 const Formulario = () => {
+    const [nombre, guardarNombre] = useState('')
+    const [cantidad, guardarCantidad] = useState(0)
+
+    // Cuando un usuario agrega un gasto
+    const agregarGasto = e => {
+        e.preventDefault()
+    }
+
     return(
-        <form>
+        <form onSubmit={agregarGasto}>
             <h2>Agrega tus gastos aquí</h2>
 
             <div className="campo">
                 <label>Nombre Gasto</label>
-                <input type="text" className="u-full-width" placeholder="Ej. Transporte"></input>
+                <input type="text" className="u-full-width" placeholder="Ej. Transporte" value={nombre} onChange={e => guardarNombre(e.target.value)}></input>
             </div>
 
             <div className="campo">
                 <label>Cantidad Gasto</label>
-                <input type="number" className="u-full-width" placeholder="Ej. 300"></input>
+                <input type="number" className="u-full-width" placeholder="Ej. 300" value={cantidad} onChange={e => guardarCantidad(parseInt(e.target.value, 10))}></input>
             </div>
 
             <input type="submit" className="button-primary u-full-width" value="Agregar Gasto"></input>
