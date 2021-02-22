d51d25a9504b8962e2bfc20dc3bafdba6259ac49
alanmartinc <alancabot.ac@gmail.com>
2021-02-22 17:25:04 -0300

    Se agrega ejercicios cargando un componente u otro

--- Sección 4 - PROYECTO Control de Presupuesto/react-fundamentos/src/App.js
+++ Sección 4 - PROYECTO Control de Presupuesto/react-fundamentos/src/App.js
@@ -6,25 +6,31 @@ function App() {
   // Definir el state
   const [presupuesto, guardarPresupuesto] = useState(0)
   const [restante, guardarRestante] = useState(0)
+  const [mostrarPregunta, actualizarPregunta] = useState(true);
 
   return (
     <div className="container">
         <header>
             <h1>Gasto Semanal</h1>
             <div className="contenido-principal contenido">
-              <Pregunta 
+              {mostrarPregunta 
+              ? ( <Pregunta 
                 guardarPresupuesto={guardarPresupuesto}
                 guardarRestante={guardarRestante}
-              />
-              <div className="row">
-                <div className="one-half column">
-                  <Formulario/>
-                </div>
+                actualizarPregunta={actualizarPregunta}
+              />) 
+              : (
+                <div className="row">
+                  <div className="one-half column">
+                    <Formulario/>
+                  </div>
 
-                <div className="one-half column">
-                  2
+                  <div className="one-half column">
+                    2
+                  </div>
                 </div>
-              </div>
+              )
+              }
             </div>
         </header>
     </div>
--- Sección 4 - PROYECTO Control de Presupuesto/react-fundamentos/src/components/Pregunta.js
+++ Sección 4 - PROYECTO Control de Presupuesto/react-fundamentos/src/components/Pregunta.js
@@ -1,7 +1,7 @@
 import React, {Fragment, useState} from 'react';
 import Error from './Error'
 
-const Pregunta = ({guardarPresupuesto, guardarRestante}) => {
+const Pregunta = ({guardarPresupuesto, guardarRestante, actualizarPregunta}) => {
     // Definir el State
     const [cantidad, guardarCantidad] = useState(0)
     const [error, guardarError] = useState(false)
@@ -25,6 +25,7 @@ const Pregunta = ({guardarPresupuesto, guardarRestante}) => {
         guardarError(false)
         guardarPresupuesto(cantidad)
         guardarRestante(cantidad)
+        actualizarPregunta(false)
     }
 
     return(
