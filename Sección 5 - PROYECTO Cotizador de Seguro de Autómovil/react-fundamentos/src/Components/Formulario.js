5125617317cf583d5dabafd5385414c96cabd380
alanmartinc <alancabot.ac@gmail.com>
2021-05-04 15:37:33 -0300

    Se agrega ejercicios calculando el costo basado en el plan

--- Sección 5 - PROYECTO Cotizador de Seguro de Autómovil/react-fundamentos/src/Components/Formulario.js
+++ Sección 5 - PROYECTO Cotizador de Seguro de Autómovil/react-fundamentos/src/Components/Formulario.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import styled from '@emotion/styled';
-import {obtenerDiferenciaYear, calcularMarca} from '../helper';
+import {obtenerDiferenciaYear, calcularMarca, obtenerPlan} from '../helper';
 
 const Campo = styled.div`
     display: flex;
@@ -90,9 +90,11 @@ const Formulario = () => {
         
         // Americano 15% - Asiatico 5% - Europeo 30%
         resultado = calcularMarca(marca) * resultado;
-        console.log(resultado);
 
         // Basíco aumenta 20% - Completo 50%
+        const incrementoPlan = obtenerPlan(plan);
+        resultado = parseFloat(incrementoPlan * resultado).toFixed(2);
+        console.log(resultado);
     }
 
     return(
