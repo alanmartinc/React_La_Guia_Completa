e4a644688b4f50e23d27d9123494ab15930eeb92
alanmartinc <alancabot.ac@gmail.com>
2021-05-04 13:14:01 -0300

    Se agrega proyecto creando los primeros Styled Components

--- Sección 5 - PROYECTO Cotizador de Seguro de Autómovil/react-fundamentos/src/App.js
+++ Sección 5 - PROYECTO Cotizador de Seguro de Autómovil/react-fundamentos/src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import Header from './Components/Header';
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`;
 
 function App() {
   return(
-    <Header titulo='Cotizador de Seguros'/>
+    <Contenedor>
+      <Header titulo='Cotizador de Seguros'/>
+    </Contenedor>
+
+    <ContenedorFormulario>
+      
+    </ContenedorFormulario>
   );
 }
 
