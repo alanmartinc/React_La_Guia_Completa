64b9090848dc9af82256da0ac5fd280b30a80dae
alanmartinc <alancabot.ac@gmail.com>
2021-05-05 14:12:22 -0300

    Se agrega ejercicios useEffect para ejecutar código una vez (o cada que algo cambie)

--- Sección 6 - PROYECTO Frases de Breaking Bad desde una API/react-fundamentos/src/App.js
+++ Sección 6 - PROYECTO Frases de Breaking Bad desde una API/react-fundamentos/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from '@emotion/styled';
 import Frase from './components/Frase'
 
@@ -18,6 +18,12 @@ const Boton = styled.button`
   padding: 1rem 3rem;
   font-size: 2rem;
   border: 2px solid black;
+  transition: background-size .8s ease;
+
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
 `;
 
 function App() {
@@ -29,6 +35,11 @@ function App() {
     const frase = await api.json()
     guardarFrase(frase[0]);
   }
+
+  // Cargar una frase
+  useEffect(() => {
+    consultarAPI()
+  }, []);
 
   return(
     <Contenedor>
