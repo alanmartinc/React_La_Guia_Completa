d423becb222f51e7fb746042aa6e212c3461027a
alanmartinc <alancabot.ac@gmail.com>
2021-05-07 18:33:08 -0300

    Se agrega ejercicios validando el formulario

--- Sección 7 - PROYECTO Obtener el Clima desde una API/react-fundamentos/src/components/Formulario.js
+++ Sección 7 - PROYECTO Obtener el Clima desde una API/react-fundamentos/src/components/Formulario.js
@@ -7,6 +7,8 @@ const Formulario = () => {
         pais: ''
     });
 
+    const [error, guardarError] = useState(false);
+
     // Extraer ciudad y pais
     const {ciudad, pais} = busqueda;
 
@@ -19,8 +21,26 @@ const Formulario = () => {
         });
     }
 
+    // Cuando el usuario da submit al form
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        // Validar
+        if(ciudad.trim() === '' || pais.trim() === '') {
+            guardarError(true);
+            return;
+        }
+
+        guardarError(false);
+
+        // Pasarlo al componente principal
+    }
+
     return(
-        <form>
+        <form
+            onSubmit={handleSubmit}
+        >
+            {error ? <p className="red darken-4 error">Todos los campos son obligatorios</p> : null}
             <div className="input-field col s12">
                 <input
                     type="text"
@@ -49,6 +69,14 @@ const Formulario = () => {
                     <option value="PE">Perú</option>
                 </select>
                 <label htmlFor="pais">País: </label>
+            </div>
+
+            <div className="input-field col s12">
+                <input
+                    type="submit"
+                    value="Buscar Clima"
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                />
             </div>
         </form>
     );
