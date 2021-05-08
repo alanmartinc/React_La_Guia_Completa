b7218a58625961f9d90abd3fe5dcfc5028b92d17
alanmartinc <alancabot.ac@gmail.com>
2021-05-08 19:56:36 -0300

    Se agrega ejercicios documentando la App

--- Sección 7 - PROYECTO Obtener el Clima desde una API/react-fundamentos/src/App.js
+++ Sección 7 - PROYECTO Obtener el Clima desde una API/react-fundamentos/src/App.js
@@ -21,7 +21,7 @@ function App() {
     const consultarAPI = async () => {
       if(consultar) {
         const appId = '0f852b668f506025a3ea0b4738a799e2';
-        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
 
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
