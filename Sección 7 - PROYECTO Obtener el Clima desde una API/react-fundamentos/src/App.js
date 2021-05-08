0b0e3f57546733c726e9993d1e2e46cd802dda1c
alanmartinc <alancabot.ac@gmail.com>
2021-05-08 19:32:56 -0300

    Se agrega ejercicios mostrando el clima y resultado de la API

--- Sección 7 - PROYECTO Obtener el Clima desde una API/react-fundamentos/src/App.js
+++ Sección 7 - PROYECTO Obtener el Clima desde una API/react-fundamentos/src/App.js
@@ -1,6 +1,7 @@
 import React, {Fragment, useState, useEffect} from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
+import Clima from './components/Clima';
 
 function App() {
   // State del formulario
@@ -10,6 +11,7 @@ function App() {
   });
 
   const [consultar, guardarConsultar] = useState(false);
+  const [resultado, guardarResultado] = useState({});
 
   const {ciudad, pais} = busqueda;
 
@@ -22,7 +24,8 @@ function App() {
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
 
-        console.log(resultado);
+        guardarResultado(resultado);
+        guardarConsultar(false);
       }
     }
     consultarAPI();
@@ -45,7 +48,9 @@ function App() {
               />
             </div>
             <div className="col m6 s12">
-              2
+              <Clima
+                resultado={resultado}
+              />
             </div>
           </div>
         </div>
---
+++ Sección 7 - PROYECTO Obtener el Clima desde una API/react-fundamentos/src/components/Clima.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Clima = ({resultado}) => {
+    // Extraer los valores
+    const {name, main} = resultado;
+
+    if(!name) return null;
+
+    // Grado kelvin
+    const kelvin = 273.15;
+
+    return(
+        <div className="card-panel white col s12">
+            <div className="black-text">
+                <h2>El clima de {name} es: </h2>
+                <p className="temperatura">
+                    {parseFloat(main.temp - kelvin, 10).toFixed(2)} <span>&#x2103;</span>
+                </p>
+                <p> Temperatura Máxima:
+                    {parseFloat(main.temp_max - kelvin, 10).toFixed(2)} <span>&#x2103;</span>
+                </p>
+                <p> Temperatura Minima:
+                    {parseFloat(main.temp_min - kelvin, 10).toFixed(2)} <span>&#x2103;</span>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Clima;
