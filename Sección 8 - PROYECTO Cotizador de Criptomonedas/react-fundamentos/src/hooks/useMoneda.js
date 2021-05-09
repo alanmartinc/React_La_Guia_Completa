6664754eeee4cccc6f23f2b8f407cf95f93acde4
alanmartinc <alancabot.ac@gmail.com>
2021-05-09 19:33:36 -0300

    Se agrega ejercicios Styled Components al Custom Hook

--- Sección 8 - PROYECTO Cotizador de Criptomonedas/react-fundamentos/src/hooks/useMoneda.js
+++ Sección 8 - PROYECTO Cotizador de Criptomonedas/react-fundamentos/src/hooks/useMoneda.js
@@ -1,4 +1,25 @@
 import React, {Fragment, useState} from 'react';
+import styled from '@emotion/styled';
+
+const Label = styled.label`
+    font-family: 'Bebas Neue', cursive;
+    color: #FFF;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 2.4rem;
+    margin-top: 2rem;
+    display: block;
+`;
+
+const Select = styled.select`
+    width: 100%;
+    display: block;
+    padding: 1rem;
+    -webkit-appearance: none;
+    border-radius: 10px;
+    border: none;
+    font-size: 1.2rem;
+`;
 
 const useMoneda = (label, stateInicial, opciones) => {
     // State de nuestro custom hook
@@ -6,8 +27,8 @@ const useMoneda = (label, stateInicial, opciones) => {
 
     const Seleccionar = () => (
         <Fragment>
-            <label>{label}</label>
-            <select
+            <Label>{label}</Label>
+            <Select
                 onChange={e => actualizarState(e.target.value)}
                 value={state}
             >
@@ -15,7 +36,7 @@ const useMoneda = (label, stateInicial, opciones) => {
                 {opciones.map(opcion => (
                     <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
                 ))}
-            </select>
+            </Select>
         </Fragment>
     );
 
