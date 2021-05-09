54530904c173e464590fcba6e0b473d4593edc93
alanmartinc <alancabot.ac@gmail.com>
2021-05-09 20:04:37 -0300

    Se agrega ejercicios Styled Component para el error

--- Sección 8 - PROYECTO Cotizador de Criptomonedas/react-fundamentos/src/components/Formulario.js
+++ Sección 8 - PROYECTO Cotizador de Criptomonedas/react-fundamentos/src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import styled from '@emotion/styled';
+import Error from './Error';
 import useMoneda from '../hooks/useMoneda';
 import useCriptomoneda from '../hooks/useCriptomoneda';
 import axios from 'axios';
@@ -70,7 +71,7 @@ const Formulario = () => {
         <form
             onSubmit={cotizarMoneda}
         >
-            {error ? 'Hay un error' : null}
+            {error ? <Error mensaje="Todos los campos son obligatorios" /> : null}
 
             <SelectMonedas/>
             <SelectCripto/>
