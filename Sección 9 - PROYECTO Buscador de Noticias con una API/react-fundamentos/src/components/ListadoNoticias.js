9338fcc101ed638e0e2b48dc722e2f30618f7373
alanmartinc <alancabot.ac@gmail.com>
2021-05-10 12:03:05 -0300

    Se agrega ejercicios documentando la API

--- Sección 9 - PROYECTO Buscador de Noticias con una API/react-fundamentos/src/components/ListadoNoticias.js
+++ Sección 9 - PROYECTO Buscador de Noticias con una API/react-fundamentos/src/components/ListadoNoticias.js
@@ -2,18 +2,16 @@ import React from 'react';
 import Noticia from './Noticia';
 import PropTypes from 'prop-types';
 
-const ListadoNoticias = ({noticias}) => {
-    return(
-        <div className="row">
-            {noticias.map(noticia => (
-                <Noticia
-                    key={noticia.url}
-                    noticia={noticia}
-                />
-            ))}
-        </div>
-    );
-}
+const ListadoNoticias = ({noticias}) => (
+    <div className="row">
+        {noticias.map(noticia => (
+            <Noticia
+                key={noticia.url}
+                noticia={noticia}
+            />
+        ))}
+    </div>
+);
 
 ListadoNoticias.prototype = {
     noticias: PropTypes.array.isRequired
