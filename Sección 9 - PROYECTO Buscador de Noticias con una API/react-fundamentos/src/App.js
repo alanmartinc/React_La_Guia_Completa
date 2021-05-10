34cf08b8459674b3e43b69fe6e90f3e5bed26571
alanmartinc <alancabot.ac@gmail.com>
2021-05-10 10:58:57 -0300

    Se agrega ejercicios consultando la API y leyendo los resultados

--- Sección 9 - PROYECTO Buscador de Noticias con una API/react-fundamentos/src/App.js
+++ Sección 9 - PROYECTO Buscador de Noticias con una API/react-fundamentos/src/App.js
@@ -1,10 +1,23 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Header from './components/Header'
 import Formulario from './components/Formulario'
 
 function App() {
   // Definir la categoria y noticias
   const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=95a018cbc0444d53889acda756ab4d53`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles);
+    }
+    consultarAPI();
+  }, [categoria]);
 
   return (
     <Fragment>
