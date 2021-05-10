8c67ecb987fdb1a502ecad8c74e4fe45b6e95331
alanmartinc <alancabot.ac@gmail.com>
2021-05-10 10:40:36 -0300

    Se agrega ejercicios mostrando las categorías en el select

--- Sección 9 - PROYECTO Buscador de Noticias con una API/react-fundamentos/src/components/Formulario.js
+++ Sección 9 - PROYECTO Buscador de Noticias con una API/react-fundamentos/src/components/Formulario.js
@@ -3,8 +3,18 @@ import styles from './Formulario.module.css';
 import useSelect from '../hooks/useSelect';
 
 const Formulario = () => {
+    const OPCIONES = [
+        {value: 'general', label: 'General'},
+        {value: 'business', label: 'Negocios'},
+        {value: 'entertainment', label: 'Entretenimiento'},
+        {value: 'health', label: 'Salud'},
+        {value: 'science', label: 'Ciencia'},
+        {value: 'sports', label: 'Deportes'},
+        {value: 'technology', label: 'Tecnología'}
+    ]
+
     // Utilizar custom hook
-    const [categoria, SelectNoticias] = useSelect();
+    const [categoria, SelectNoticias] = useSelect('general', OPCIONES);
 
     return(
         <div className={`${styles.buscador} row`}>
@@ -13,7 +23,7 @@ const Formulario = () => {
                     <h2 className={styles.heading}>Encuentra Noticias por Categoría</h2>
 
                     <SelectNoticias/>
-                    
+
                     <div className="input-field col s12">
                         <input
                             type="submit"
