e9c1d4caf6130ead24264034316e7d1c986d5500
alanmartinc <alancabot.ac@gmail.com>
2021-05-10 11:46:11 -0300

    Se agrega ejercicios mostrando las noticias

--- Sección 9 - PROYECTO Buscador de Noticias con una API/react-fundamentos/src/components/ListadoNoticias.js
+++ Sección 9 - PROYECTO Buscador de Noticias con una API/react-fundamentos/src/components/ListadoNoticias.js
@@ -5,7 +5,10 @@ const ListadoNoticias = ({noticias}) => {
     return(
         <div className="row">
             {noticias.map(noticia => (
-                <Noticia/>
+                <Noticia
+                    key={noticia.url}
+                    noticia={noticia}
+                />
             ))}
         </div>
     );
--- Sección 9 - PROYECTO Buscador de Noticias con una API/react-fundamentos/src/components/Noticia.js
+++ Sección 9 - PROYECTO Buscador de Noticias con una API/react-fundamentos/src/components/Noticia.js
@@ -1,8 +1,36 @@
 import React from 'react';
 
-const Noticia = () => {
+const Noticia = ({noticia}) => {
+    // Extraer los datos
+    const {urlToImage, url, title, description, source} = noticia;
+
+    const imagen = (urlToImage) ?
+        <div className="card-image">
+            <img src={urlToImage} alt={title}/>
+            <span className="card-title">{source.name}</span>
+        </div>
+    : null;
+
     return(
-        <p>Noticia</p>
+        <div className="col s12 m6 l4">
+            <div className="card">
+                {imagen}
+
+                <div className="card-content">
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                </div>
+
+                <div className="card-action">
+                    <a
+                        href={url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="waves-effect waves-light btn"
+                    >Ver Noticia Completa</a>
+                </div>
+            </div>
+        </div>
     );
 }
 
