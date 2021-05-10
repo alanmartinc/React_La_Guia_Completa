5fa4eadeda48f8f5b3cb1cedfdba8401eb31d158
alanmartinc <alancabot.ac@gmail.com>
2021-05-10 10:25:28 -0300

    Se agrega ejercicios creando un Hook para el select

--- Sección 9 - PROYECTO Buscador de Noticias con una API/react-fundamentos/src/components/Formulario.js
+++ Sección 9 - PROYECTO Buscador de Noticias con una API/react-fundamentos/src/components/Formulario.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import styles from './Formulario.module.css';
+import useSelect from '../hooks/useSelect';
 
 const Formulario = () => {
+    // Utilizar custom hook
+    const [categoria, SelectNoticias] = useSelect();
+
     return(
         <div className={`${styles.buscador} row`}>
             <div className="col s12 m8 offset-m2">
                 <form>
                     <h2 className={styles.heading}>Encuentra Noticias por Categoría</h2>
 
+                    <SelectNoticias/>
+                    
                     <div className="input-field col s12">
                         <input
                             type="submit"
---
+++ Sección 9 - PROYECTO Buscador de Noticias con una API/react-fundamentos/src/hooks/useSelect.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+const useSelect = (stateInicial, opciones) => {
+    // State del custom hook
+    const [state, actualizarState] = useState('');
+    
+    const SelectNoticias = () => (
+        <select
+            className="browser-default"
+        >
+            <option value="">Seleccione</option>
+        </select>
+    );
+
+    return [state, SelectNoticias];
+}
+
+export default useSelect;
