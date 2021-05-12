b9912b7c320a84a9bd873b75afde44d52e00dd38
alanmartinc <alancabot.ac@gmail.com>
2021-05-12 16:45:02 -0300

    Se agrega ejercicios creando el listado de imágenes

--- Sección 10 - PROYECTO Buscador de Imágenes con Pixabay API/react-fundamentos/src/App.js
+++ Sección 10 - PROYECTO Buscador de Imágenes con Pixabay API/react-fundamentos/src/App.js
@@ -1,20 +1,22 @@
 import React, {useState, useEffect} from 'react';
 import Formulario from './components/Formulario';
+import ListadoImagenes from './components/ListadoImagenes';
 
 function App() {
   // State de la app
   const [busqueda, guardarBusqueda] = useState('');
+  const [imagenes, guardarImagenes] = useState([]);
 
   useEffect(() => {
     if(busqueda === '') return;
     const consultarApi = async () => {
-      const imagenesPorPagina = 30;
+      const imagenesPorPagina = 12;
       const key = '21588964-8c9102f5b4a4ebdcabf3a17cb';
       const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
 
       const respuesta = await fetch(url);
       const resultado = await respuesta.json();
-      guardarBusqueda(resultado.hits);
+      guardarImagenes(resultado.hits);
     }
     consultarApi();
   }, [busqueda])
@@ -26,6 +28,12 @@ function App() {
 
         <Formulario
           guardarBusqueda={guardarBusqueda}
+        />
+      </div>
+
+      <div className="row justify-content-center">
+        <ListadoImagenes
+          imagenes={imagenes}
         />
       </div>
     </div>
---
+++ Sección 10 - PROYECTO Buscador de Imágenes con Pixabay API/react-fundamentos/src/components/Imagen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Imagen = ({imagen}) => {
+    // Extraer las variables
+    const {largeImageURL, likes, previewURL, tags, views} = imagen;
+
+    return(
+        <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+            <div className="card">
+                <img src={previewURL} alt={tags} className="card-img-top"/>
+            </div>
+        </div>
+    );
+}
+
+export default Imagen;
