180b373de6f477e93aca2eb7fad7f1fa84c05093
alanmartinc <alancabot.ac@gmail.com>
2021-05-13 10:26:26 -0300

    Se agrega proyecto Buscador de Letras e Información de Artistas

---
+++ Sección 11 - Buscador de Letras e Información de Artistas/react-fundamentos/src/App.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+import Formulario from './components/Formulario';
+
+function App() {
+  return (
+    <Fragment>
+      <Formulario/>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ Sección 11 - Buscador de Letras e Información de Artistas/react-fundamentos/src/components/Formulario.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Formulario = () => {
+    return(
+        <h1>Formulario</h1>
+    );
+}
+
+export default Formulario;
