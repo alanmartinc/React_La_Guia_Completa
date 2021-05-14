2f84dbedd13e03914a015061012139b16699bfe1
alanmartinc <alancabot.ac@gmail.com>
2021-05-14 12:34:27 -0300

    Se agrega ejercicios leyendo la información en el State

--- Sección 11 - Buscador de Letras e Información de Artistas/react-fundamentos/src/components/Formulario.js
+++ Sección 11 - Buscador de Letras e Información de Artistas/react-fundamentos/src/components/Formulario.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Formulario = () => {
+    const [busqueda, guardarBusqueda] = useState({
+        artista: '',
+        cancion: ''
+    });
+
+    const [error, guardarError] = useState(false);
+
+    const {artista, cancion} = busqueda;
+
+    // Función a cada input para leer su contenido
+    const actualizarState = e => {
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    // Consultar las apis
+    const buscarInformacion = e => {
+        e.preventDefault();
+
+        if(artista.trim() === '' || cancion.trim() === '') {
+            guardarError(true);
+            return;
+        }
+        
+        guardarError(false);
+        // Todo bien, pasar al componente principal
+    }
+
     return(
         <div className="bg-info">
             <div className="container">
                 <div className="row">
-                    <form className="col card text-white bg-transparent mb-5 pt-5 pb-2">
+                    <form
+                        onSubmit={buscarInformacion}
+                        className="col card text-white bg-transparent mb-5 pt-5 pb-2"
+                    >
                         <fieldset>
                             <legend className="text-center">
                                 Buscador Letras Canciones
@@ -20,6 +53,8 @@ const Formulario = () => {
                                             className="form-control"
                                             name="artista"
                                             placeholder="Nombre Artista"
+                                            onChange={actualizarState}
+                                            value={artista}
                                         />
                                     </div>
                                 </div>
@@ -32,6 +67,8 @@ const Formulario = () => {
                                             className="form-control"
                                             name="cancion"
                                             placeholder="Nombre Canción"
+                                            onChange={actualizarState}
+                                            value={cancion}
                                         />
                                     </div>
                                 </div>
