f9510e1995c5d6748d93144d9ec9b33af8ad1330
alanmartinc <alancabot.ac@gmail.com>
2021-05-18 23:21:08 -0300

    Se agrega proyecto obteniendo la letra de la canción

--- Sección 11 - Buscador de Letras e Información de Artistas/react-fundamentos/src/App.js
+++ Sección 11 - Buscador de Letras e Información de Artistas/react-fundamentos/src/App.js
@@ -1,10 +1,29 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Formulario from './components/Formulario';
+import axios from 'axios';
 
 function App() {
+  // Definir el state
+  const [busquedaLetra, guardarBusquedaLetra] = useState({});
+  const [letra, guardarLetra] = useState('');
+
+  useEffect(() => {
+    if(Object.keys(busquedaLetra).length === 0) return;
+
+    const consultarApiLetra = async () => {
+      const {artista, cancion} = busquedaLetra;
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+      const resultado = await axios(url);
+      guardarLetra(resultado.data.lyrics);
+    }
+    consultarApiLetra();
+  }, [busquedaLetra]);
+
   return (
     <Fragment>
-      <Formulario/>
+      <Formulario
+        guardarBusquedaLetra={guardarBusquedaLetra}
+      />
     </Fragment>
   );
 }
--- Sección 11 - Buscador de Letras e Información de Artistas/react-fundamentos/src/components/Formulario.js
+++ Sección 11 - Buscador de Letras e Información de Artistas/react-fundamentos/src/components/Formulario.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 
-const Formulario = () => {
+const Formulario = ({guardarBusquedaLetra}) => {
     const [busqueda, guardarBusqueda] = useState({
         artista: '',
         cancion: ''
@@ -29,10 +29,13 @@ const Formulario = () => {
         
         guardarError(false);
         // Todo bien, pasar al componente principal
+
+        guardarBusquedaLetra(busqueda);
     }
 
     return(
         <div className="bg-info">
+            {error ? <p className="alert alert-danger text-center p-2">Todos los campos son obligatorios</p> : null}
             <div className="container">
                 <div className="row">
                     <form
