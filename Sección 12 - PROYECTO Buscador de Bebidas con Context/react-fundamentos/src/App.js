c76451d41c61725edb67628352f7158396bbc264
alanmartinc <alancabot.ac@gmail.com>
2021-05-19 13:54:41 -0300

    Se agrega proyecto Buscador de Bebidas con Context

---
+++ Sección 12 - PROYECTO Buscador de Bebidas con Context/react-fundamentos/src/App.js
@@ -0,0 +1,19 @@
+import React, {Fragment} from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+
+function App() {
+  return (
+    <Fragment>
+      <Header/>
+
+      <div className="container mt-5">
+        <div className="row">
+          <Formulario/>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ Sección 12 - PROYECTO Buscador de Bebidas con Context/react-fundamentos/src/components/Formulario.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Formulario = () => {
+    return(
+        <h1>Formulario aquí</h1>
+    );
+}
+
+export default Formulario
