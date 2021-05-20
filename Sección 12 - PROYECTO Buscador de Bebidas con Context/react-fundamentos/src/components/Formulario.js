79bc411c74d5c0d39939c1d2177bcb20c27fabd3
alanmartinc <alancabot.ac@gmail.com>
2021-05-19 23:41:18 -0300

    Se agrega ejercicios mostrando las opciones en el formulario

--- Sección 12 - PROYECTO Buscador de Bebidas con Context/react-fundamentos/src/components/Formulario.js
+++ Sección 12 - PROYECTO Buscador de Bebidas con Context/react-fundamentos/src/components/Formulario.js
@@ -25,6 +25,12 @@ const Formulario = () => {
                         name="categoria"
                     >
                         <option value="">-- Selecciona Categoría --</option>
+                        {categorias.map(categoria => (
+                            <option 
+                            key={categoria.strCategory} 
+                            value={categoria.strCategory}
+                            >{categoria.strCategory}</option>
+                        ))}
                     </select>
                 </div>
                 <div className="col-md-4">
