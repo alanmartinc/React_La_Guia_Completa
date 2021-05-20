40537bf2ccd335dc3081c30d2d795b53ef1acced
alanmartinc <alancabot.ac@gmail.com>
2021-05-20 00:51:44 -0300

    Se agrega ejercicios leer lo que el usuario selecciona

--- Sección 12 - PROYECTO Buscador de Bebidas con Context/react-fundamentos/src/components/Formulario.js
+++ Sección 12 - PROYECTO Buscador de Bebidas con Context/react-fundamentos/src/components/Formulario.js
@@ -1,8 +1,21 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {CategoriasContext} from '../context/CategoriasContext';
 
 const Formulario = () => {
+    const [busqueda, guardarBusqueda] = useState({
+        nombre: '',
+        categoria: ''
+    });
+
     const {categorias} = useContext(CategoriasContext);
+
+    // Función para leer los contenidos
+    const obtenerDatosReceta = e => {
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value
+        });
+    }
 
     return(
         <form className="col-12">
@@ -17,12 +30,14 @@ const Formulario = () => {
                         className="form-control"
                         type="text"
                         placeholder="Buscar por Ingrediente"
+                        onChange={obtenerDatosReceta}
                     />
                 </div>
                 <div className="col-md-4">
                     <select 
                         className="form-control"
                         name="categoria"
+                        onChange={obtenerDatosReceta}
                     >
                         <option value="">-- Selecciona Categoría --</option>
                         {categorias.map(categoria => (
