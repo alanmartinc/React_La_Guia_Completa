591e8dfccf47e209ab3858b065a683ee015fb8d9
alanmartinc <alancabot.ac@gmail.com>
2021-05-22 02:46:57 -0300

    Se agrega ejercicios consultando la API para obtener la receta en especifico

--- Sección 12 - PROYECTO Buscador de Bebidas con Context/react-fundamentos/src/context/ModalContext.js
+++ Sección 12 - PROYECTO Buscador de Bebidas con Context/react-fundamentos/src/context/ModalContext.js
@@ -7,6 +7,18 @@ export const ModalContext = createContext();
 const ModalProvider = (props) => {
     // State del provider
     const [idreceta, guardarIdReceta] = useState(null);
+    const [receta, guardarReceta] = useState({});
+
+    // Una vez que tenemos una receta, llamar la api
+    useEffect(() => {
+        const obtenerReceta = async () => {
+            if(!idreceta) return;
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}`;
+            const resultado = await axios.get(url);
+            guardarReceta(resultado.data.drinks[0]);
+        }
+        obtenerReceta();
+    }, [idreceta]);
 
     return(
         <ModalContext.Provider
