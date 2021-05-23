5c3659fedf480765cfed09a1d0e06067a179b014
alanmartinc <alancabot.ac@gmail.com>
2021-05-23 09:44:44 -0300

    Se agrega ejercicios State y funciones para el componente

--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/components/auth/Login.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/components/auth/Login.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const Login = () => {
-    const onChange = () => {
+    // State para iniciar sesión
+    const [usuario, guardarUsuario] = useState({
+        email: '',
+        password: ''
+    });
 
+    // Extraer de usuario
+    const {email, password} = usuario;
+
+    const onChange = e => {
+        guardarUsuario({
+            ...usuario,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    // Cuando el usuario quiere iniciar sesión
+    const onSubmit = e => {
+        e.preventDefault();
+
+        // Validar que no haya campos vacios
+
+        // Pasarlo al action
     }
 
     return(
@@ -10,7 +32,9 @@ const Login = () => {
             <div className="contenedor-form sombra-dark">
                 <h1>Iniciar Sesión</h1>
 
-                <form>
+                <form
+                    onSubmit={onSubmit}
+                >
                     <div className="campo-form">
                         <label htmlFor="email">Email</label>
                         <input
@@ -18,6 +42,7 @@ const Login = () => {
                             id="email"
                             name="email"
                             placeholder="Tu Email"
+                            value={email}
                             onChange={onChange}
                         />
                     </div>
@@ -29,6 +54,7 @@ const Login = () => {
                             id="password"
                             name="password"
                             placeholder="Tu Password"
+                            value={password}
                             onChange={onChange}
                         />
                     </div>
@@ -37,6 +63,10 @@ const Login = () => {
                         <input type="submit" className="btn btn-primario btn-block" value="Iniciar Sesión"/>
                     </div>
                 </form>
+
+                <Link to={'/nueva-cuenta'} className="enlace-cuenta">
+                    Obtener Cuenta
+                </Link>
             </div>
         </div>
     );
