1910f8edfbeaa89c0f09699034a159e937b6bb6f
alanmartinc <alancabot.ac@gmail.com>
2021-05-23 08:52:01 -0300

    Se agrega ejercicios creando el formulario de login

--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/components/auth/Login.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/components/auth/Login.js
@@ -1,8 +1,44 @@
 import React from 'react';
 
 const Login = () => {
+    const onChange = () => {
+
+    }
+
     return(
-        <h1>Desde Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sesión</h1>
+
+                <form>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Tu Email"
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Tu Password"
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <input type="submit" className="btn btn-primario btn-block" value="Iniciar Sesión"/>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
 
