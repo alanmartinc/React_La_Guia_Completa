8b61eb378f04c43b419baa051ef6e22d1bef777c
alanmartinc <alancabot.ac@gmail.com>
2021-05-23 11:11:46 -0300

    Se agrega ejercicios State y funciones para nuevos proyectos

--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/components/proyectos/NuevoProyecto.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/components/proyectos/NuevoProyecto.js
@@ -1,6 +1,33 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 
 const NuevoProyecto = () => {
+    // State para proyecto
+    const [proyecto, guardarProyecto] = useState({
+        nombre: ''
+    });
+
+    // Extraer nombre de proyecto
+    const {nombre} = proyecto;
+
+    // Lee los contenidos del input
+    const onChangeProyecto = e => {
+        guardarProyecto({
+            ...proyecto,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    // Cuando el usuario envia un proyecto
+    const onSubmitProyecto = e => {
+        e.preventDefault();
+
+        // Validar el proyecto
+
+        // Agregar al state
+
+        // Reiniciar el form
+    }
+
     return(
         <Fragment>
             <button
@@ -12,12 +39,15 @@ const NuevoProyecto = () => {
 
             <form
                 className="formulario-nuevo-proyecto"
+                onSubmit={onSubmitProyecto}
             >
                 <input
                     type="text"
                     className="input-text"
                     placeholder="Nombre Proyecto"
                     name="nombre"
+                    value={nombre}
+                    onChange={onChangeProyecto}
                 />
 
                 <input
