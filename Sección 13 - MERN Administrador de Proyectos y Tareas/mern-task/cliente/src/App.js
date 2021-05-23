2aa275812b740efd28d71ec2ae0303e979ebfb4c
alanmartinc <alancabot.ac@gmail.com>
2021-05-23 08:46:02 -0300

    Se agrega ejercicios creando los componentes y routing

--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/App.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Login from './components/auth/Login';
+import NuevaCuenta from './components/auth/NuevaCuenta';
+import Proyectos from './components/proyectos/Proyectos';
 
 function App() {
   return (
-    <h1>MERNTasks</h1>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login}/>
+        <Route exact path="/nueva-cuenta" component={NuevaCuenta}/>
+        <Route exact path="/proyectos" component={Proyectos}/>
+      </Switch>
+    </Router>
   );
 }
 
