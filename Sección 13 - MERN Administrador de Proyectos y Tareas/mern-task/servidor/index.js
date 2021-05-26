617b346c73924f421bb6bdfcafc5dd016cec44f8
alanmartinc <alancabot.ac@gmail.com>
2021-05-26 19:39:44 -0300

    Se agrega ejercicios configurando rutas de usuarios

--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/servidor/index.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/servidor/index.js
@@ -10,6 +10,9 @@ conectarDB();
 // Puerto de la app
 const PORT = process.env.PORT || 4000;
 
+// Importar rutas
+app.use('/api/usuarios', require('./routes/usuarios'));
+
 // Arrancar la app
 app.listen(PORT, () => {
     console.log(`El servidor esta funcionando en el puerto ${PORT}`);
