1bf5f21f5f80afaa7bfe6c89ba8001e190fd28c9
alanmartinc <alancabot.ac@gmail.com>
2021-05-26 03:59:57 -0300

    Se agrega ejercicios agregando los proyectos con context

--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/components/proyectos/ListadoProjectos.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/components/proyectos/ListadoProjectos.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Proyecto from './Projecto';
+import ProyectoContext from '../../context/proyectos/ProyectoContext';
 
 const ListadoProjectos = () => {
-    const proyectos = [
-        {nombre: 'Tienda Virtual'},
-        {nombre: 'Intranet'},
-        {nombre: 'Diseño de Sitio Web'}
-    ]
+    // Extraer proyectos de state inicial
+    const proyectosContext = useContext(ProyectoContext);
+    const {proyectos} = proyectosContext;
+
+    // Revisar si proyectos tiene contenido
+    if(proyectos.length === 0) return null;
 
     return(
         <ul className="listado-proyectos">
             {proyectos.map(proyecto => (
                 <Proyecto
+                    key={proyecto.id}
                     proyecto={proyecto}
                 />
             ))}
--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/context/proyectos/ProyectoState.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/context/proyectos/ProyectoState.js
@@ -5,6 +5,12 @@ import {FORMULARIO_PROYECTO} from '../../types';
 
 const ProyectoState = props => {
     const initialState = {
+        proyectos : [
+            {id: 1, nombre: 'Tienda Virtual'},
+            {id: 2, nombre: 'Intranet'},
+            {id: 3, nombre: 'Diseño de Sitio Web'},
+            {id: 4, nombre: 'MERN'}
+        ],
         formulario : false
     }
 
@@ -21,6 +27,7 @@ const ProyectoState = props => {
     return (
         <ProyectoContext.Provider
             value={{
+                proyectos: state.proyectos,
                 formulario: state.formulario,
                 mostrarFormulario
             }}
