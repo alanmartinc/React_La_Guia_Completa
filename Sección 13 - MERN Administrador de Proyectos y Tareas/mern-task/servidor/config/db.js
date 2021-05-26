da9abef9c3d3441b6bb9103074364ba75394a204
alanmartinc <alancabot.ac@gmail.com>
2021-05-26 15:59:03 -0300

    Se agrega ejercicios conectando MongoDB a nuestra aplicación

---
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/servidor/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require('dotenv').config({path: 'variables.env'});
+
+const conectarDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_MONGO, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        });
+        console.log('DB Conectada');
+    } catch (error) {
+        console.log(error);
+        process.exit(1); // Detener la app
+    }
+}
+
+module.export = conectarDB;
--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/servidor/index.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/servidor/index.js
@@ -1,15 +1,14 @@
 const express = require('express');
+const conectarDB = require('./config/db');
 
 // Crear el servidor
 const app = express();
 
+// Conectar a la base de datos
+conectarDB();
+
 // Puerto de la app
 const PORT = process.env.PORT || 4000;
-
-// Definir la página principal
-app.get('/', (req, res) => {
-    res.send('Hola Mundo');
-});
 
 // Arrancar la app
 app.listen(PORT, () => {
