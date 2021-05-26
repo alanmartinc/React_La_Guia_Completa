5cb795339404b3774a3778df1b59b5890962d2b9
alanmartinc <alancabot.ac@gmail.com>
2021-05-26 05:28:59 -0300

    Se agrega ejercicios mostrando el nombre del proyecto

--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/components/tareas/ListadoTareas.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/components/tareas/ListadoTareas.js
@@ -1,7 +1,18 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useContext} from 'react';
 import Tarea from './Tarea';
+import ProyectoContext from '../../context/proyectos/ProyectoContext';
 
 const ListadoTareas = () => {
+    // Obtener el state de 
+    const proyectosContext = useContext(ProyectoContext);
+    const {proyecto} = proyectosContext;
+
+    // Si no hay proyecto seleccionado
+    if(!proyecto) return <h2>Selecciona un proyecto</h2>
+
+    // Array destructuring para extraer el proyecto actual
+    const [proyectoActual] = proyecto;
+
     const tareasProyecto = [
         {nombre: 'Elegir Plataforma', estado: true},
         {nombre: 'Elegir Colores', estado: false},
@@ -11,7 +22,7 @@ const ListadoTareas = () => {
 
     return(
         <Fragment>
-            <h2>Proyecto: Tienda Virtual</h2>
+            <h2>Proyecto: {proyectoActual.nombre}</h2>
 
             <ul className="listado-tareas">
                 {tareasProyecto.length === 0 
