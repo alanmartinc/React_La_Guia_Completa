56fc805f9c61a42ed0020c901ea104ee5d8575ce
alanmartinc <alancabot.ac@gmail.com>
2021-05-26 12:32:09 -0300

    Se agrega ejercicios creando el servidor de Express

--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/servidor/index.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/servidor/index.js
@@ -1,1 +1,17 @@
-console.log('Desde index.js');
+const express = require('express');
+
+// Crear el servidor
+const app = express();
+
+// Puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// Definir la página principal
+app.get('/', (req, res) => {
+    res.send('Hola Mundo');
+});
+
+// Arrancar la app
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+});
