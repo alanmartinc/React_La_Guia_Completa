1d496089247107b93d4a7d595e8fcea9cf93a059
alanmartinc <alancabot.ac@gmail.com>
2021-05-26 07:01:26 -0300

    Se agrega ejercicios insertando tareas en el proyecto actual

--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/components/tareas/FormTarea.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/components/tareas/FormTarea.js
@@ -1,16 +1,37 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import ProyectoContext from '../../context/proyectos/ProyectoContext';
+import TareaContext from '../../context/tareas/TareaContext';
 
 const FormTarea = () => {
     // Obtener si un proyecto esta activo
     const proyectosContext = useContext(ProyectoContext);
     const {proyecto} = proyectosContext;
 
+    // Obtener la función del context de tarea
+    const tareasContext = useContext(TareaContext);
+    const {agregarTarea} = tareasContext;
+
+    // State del formulario
+    const [tarea, guardarTarea] = useState({
+        nombre: ''
+    });
+
+    // Extraer el nombre del proyecto
+    const {nombre} = tarea;
+
     // Si no hay proyecto seleccionado
     if(!proyecto) return null;
 
     // Array destructuring para extraer el proyecto actual
     const [proyectoActual] = proyecto;
+
+    // Leer los valores del formulario
+    const handleChange = e => {
+        guardarTarea({
+            ...tarea,
+            [e.target.name]: e.target.value
+        })
+    }
 
     const onSubmit = e => {
         e.preventDefault();
@@ -20,6 +41,9 @@ const FormTarea = () => {
         // Pasar la validación
 
         // Agregar la nueva tarea al state de tareas
+        tarea.proyectoId = proyectoActual.id;
+        tarea.estado = false;
+        agregarTarea(tarea);
 
         // Reiniciar el form
     }
@@ -35,6 +59,8 @@ const FormTarea = () => {
                         className="input-text"
                         placeholder="Nombre Tarea..."
                         name="nombre"
+                        value={nombre}
+                        onChange={handleChange}
                     />
                 </div>
 
