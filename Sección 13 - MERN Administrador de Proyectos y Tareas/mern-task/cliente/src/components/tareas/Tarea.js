f5630280c5ab384e2b0fce416c08ffe6e7f2f89a
alanmartinc <alancabot.ac@gmail.com>
2021-05-26 03:12:59 -0300

    Se agrega ejercicios trabajando con la interfaz de tareas

--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/components/tareas/Tarea.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/components/tareas/Tarea.js
@@ -1,8 +1,41 @@
 import React from 'react';
 
-const Tarea = () => {
+const Tarea = ({tarea}) => {
     return(
-        <p>Desde Tarea</p>
+        <li className="tarea sombra">
+            <p>{tarea.nombre}</p>
+
+            <div className="estado">
+                {tarea.estado
+                ? 
+                    (
+                        <button
+                            type="button"
+                            className="completo"
+                        >Completo</button>
+                    )
+
+                :   (
+                        <button
+                            type="button"
+                            className="incompleto"
+                        >Incompleto</button>
+                    )
+                }
+            </div>
+
+            <div className="acciones">
+                <button
+                    type="button"
+                    className="btn btn-primario"
+                >Editar</button>
+
+                <button
+                    type="button"
+                    className="btn btn-secundario"
+                >Eliminar</button>
+            </div>
+        </li>
     );
 }
 
