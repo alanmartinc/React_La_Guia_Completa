059e8dbd21afa5505ccb73c347663638f1a276bd
alanmartinc <alancabot.ac@gmail.com>
2021-05-27 16:29:01 -0300

    Se agrega ejercicios creando un JWT

--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/servidor/controllers/usuarioController.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/servidor/controllers/usuarioController.js
@@ -1,6 +1,7 @@
 const Usuario = require('../models/Usuario');
 const bcryptjs = require('bcryptjs');
 const {validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken');
 
 exports.crearUsuario = async (req, res) => {
     // Revisar si hay errores
@@ -30,8 +31,22 @@ exports.crearUsuario = async (req, res) => {
         // Guardar usuario
         await usuario.save();
 
-        // Mensaje de confirmación
-        res.json({msg: 'Usuario creado correctamente'});
+        // Crear y firmar el JWT
+        const payload = {
+            usuario: {
+                id: usuario.id
+            }
+        };
+
+        // Firmar el JWT
+        jwt.sign(payload, process.env.SECRETA, {
+            expiresIn: 3600 // 1 hora
+        }, (error, token) => {
+            if(error) throw error;
+
+            // Mensaje de confirmación
+            res.json({token});
+        });
     } catch (error) {
         console.log(error);
         res.status(400).send('Hubo un error');
