4da33051170226e0f352a55371a8c68cdf48bb5d
alanmartinc <alancabot.ac@gmail.com>
2021-05-30 22:56:17 -0300

    Se agrega ejercicios revisando si un proyecto existe o no

--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/servidor/controllers/proyectoController.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/servidor/controllers/proyectoController.js
@@ -53,12 +53,21 @@ exports.actualizarProyecto = async (req, res) => {
 
     try {
         // Revisar el ID
+        let proyecto = await Proyecto.findById(req.params.id);
 
         // Si el proyecto existe o no
+        if(!proyecto) {
+            return res.status(404).json({msg: 'Proyecto no encontrado'});
+        }
 
         // Verificar el creador del proyecto
+        if(proyecto.creador.toString() !== req.usuario.id) {
+            return res.status(401).json({msg: 'No Autorizado'});
+        }
 
         // Actualizar
+        proyecto = await Proyecto.findByIdAndUpdate({_id: req.params.id}, {$set: nuevoProyecto}, {new: true});
+        res.json({proyecto});
     } catch (error) {
         console.log(error);
         res.status(500).send('Error en el servidor');
