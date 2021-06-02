ac4491dd07a6f5de1661724a8cafaf04b82371bc
alanmartinc <alancabot.ac@gmail.com>
2021-06-02 01:31:24 -0300

    Se agrega ejercicios creando el State y Provider

--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/App.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/App.js
@@ -6,19 +6,22 @@ import Proyectos from './components/proyectos/Proyectos';
 import ProyectoState from './context/proyectos/ProyectoState';
 import TareaState from './context/tareas/TareaState';
 import AlertaState from './context/alertas/AlertaState';
+import AuthState from './context/autenticacion/AuthState';
 
 function App() {
   return (
     <ProyectoState>
       <TareaState>
         <AlertaState>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Login}/>
-              <Route exact path="/nueva-cuenta" component={NuevaCuenta}/>
-              <Route exact path="/proyectos" component={Proyectos}/>
-            </Switch>
-          </Router>
+          <AuthState>
+            <Router>
+              <Switch>
+                <Route exact path="/" component={Login}/>
+                <Route exact path="/nueva-cuenta" component={NuevaCuenta}/>
+                <Route exact path="/proyectos" component={Proyectos}/>
+              </Switch>
+            </Router>
+          </AuthState>
         </AlertaState>
       </TareaState>
     </ProyectoState>
---
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/context/autenticacion/AuthState.js
@@ -0,0 +1,40 @@
+import React, {useReducer} from 'react';
+import AuthReducer from './AuthReducer';
+import AuthContext from './AuthContext';
+import {
+    REGISTRO_EXITOSO,
+    REGISTRO_ERROR,
+    OBTENER_USUARIO,
+    LOGIN_EXITOSO,
+    LOGIN_ERROR,
+    CERRAR_SESION
+} from '../../types';
+
+const AuthState = props => {
+    const initialState = {
+        token: localStorage.getItem('token'),
+        autenticado: null,
+        usuario: null,
+        mensaje: null
+    }
+
+    const [state, dispatch] = useReducer(AuthReducer, initialState);
+
+    // Las funciones
+
+
+    return(
+        <AuthContext.Provider
+            value={{
+                token: state.token,
+                autenticado: state.autenticado,
+                usuario: state.usuario,
+                mensaje: state.mensaje
+            }}
+        >
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthState;
