1f450a9b10979838e8fea75d219ff48b49f42cdb
alanmartinc <alancabot.ac@gmail.com>
2021-06-02 01:10:54 -0300

    Se agrega ejercicios validando que no haya campos vacios y mostrando alertas

--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/components/auth/NuevaCuenta.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/components/auth/NuevaCuenta.js
@@ -1,7 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import {Link} from 'react-router-dom';
+import AlertaContext from '../../context/alertas/AlertaContext';
 
 const NuevaCuenta = () => {
+    // Extraer los valores del context
+    const alertaContext = useContext(AlertaContext);
+    const {alerta, mostrarAlerta} = alertaContext;
+
     // State para iniciar sesión
     const [usuario, guardarUsuario] = useState({
         nombre: '',
@@ -25,6 +30,12 @@ const NuevaCuenta = () => {
         e.preventDefault();
 
         // Validar que no haya campos vacios
+        if( nombre.trim() === '' || 
+            email.trim() === '' || 
+            password.trim() === '' || 
+            confirmar.trim() === '' ) {
+                mostrarAlerta('Todos los campos son obligatorios', 'alerta-error');
+            }
 
         // Password mínimo de 6 carácteres
 
@@ -35,6 +46,7 @@ const NuevaCuenta = () => {
 
     return(
         <div className="form-usuario">
+            {alerta ? (<div className={`alerta ${alerta.categoria}`}>{alerta.msg}</div>) : null}
             <div className="contenedor-form sombra-dark">
                 <h1>Obtener una cuenta</h1>
 
