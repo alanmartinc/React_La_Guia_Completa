58864de23f7a4e8d0be9a098414e3696a16daa2f
alanmartinc <alancabot.ac@gmail.com>
2021-06-02 18:22:01 -0300

    Se agrega ejercicios proteger componentes

--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/App.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/cliente/src/App.js
@@ -8,6 +8,7 @@ import TareaState from './context/tareas/TareaState';
 import AlertaState from './context/alertas/AlertaState';
 import AuthState from './context/autenticacion/AuthState';
 import tokenAuth from './config/token';
+import RutaPrivada from './components/rutas/RutaPrivada';
 
 // Revisar si tenemos un token
 const token = localStorage.getItem('token');
@@ -25,7 +26,7 @@ function App() {
               <Switch>
                 <Route exact path="/" component={Login}/>
                 <Route exact path="/nueva-cuenta" component={NuevaCuenta}/>
-                <Route exact path="/proyectos" component={Proyectos}/>
+                <RutaPrivada exact path="/proyectos" component={Proyectos}/>
               </Switch>
             </Router>
           </AuthState>
