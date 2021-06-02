c817af6324f5fceaf9982bea57db9fe962b12502
alanmartinc <alancabot.ac@gmail.com>
2021-06-02 02:10:27 -0300

    Se agrega ejercicios habilitando Cors y creando usuario

--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/servidor/index.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/servidor/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const conectarDB = require('./config/db');
+const cors = require('cors');
 
 // Crear el servidor
 const app = express();
 
 // Conectar a la base de datos
 conectarDB();
+
+// Habilitar cors
+app.use(cors());
 
 // Habilitar express.json
 app.use(express.json({extended: true}));
