5a83ca532ad83e056528114200d377bbea3ffd77
alanmartinc <alancabot.ac@gmail.com>
2021-06-03 01:09:28 -0300

    Se agrega obteniendo las tareas del proyecto

--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/servidor/controllers/tareaController.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/servidor/controllers/tareaController.js
@@ -38,7 +38,7 @@ exports.crearTarea = async (req, res) => {
 exports.obtenerTareas = async (req, res) => {
     try {
         // Extraer el proyecto y comprobar si existe
-        const {proyecto} = req.body;
+        const {proyecto} = req.query;
 
         const existeProyecto = await Proyecto.findById(proyecto);
         if(!existeProyecto) {
@@ -51,7 +51,7 @@ exports.obtenerTareas = async (req, res) => {
         }
 
         // Obtener las tareas por proyecto
-        const tareas = await Tarea.find({proyecto});
+        const tareas = await Tarea.find({proyecto}).sort({creado: -1});
         res.json({tareas});
     } catch (error) {
         console.log(error);
