90e1ab1c329ee2777679a383544204febdf957a8
alanmartinc <alancabot.ac@gmail.com>
2021-06-03 14:11:36 -0300

    Se arregla cambios para el deploy del proyecto

--- Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/servidor/index.js
+++ Sección 13 - MERN Administrador de Proyectos y Tareas/mern-task/servidor/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({extended: true}));
 
 // Puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 // Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -24,6 +24,6 @@ app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
 // Arrancar la app
-app.listen(PORT, () => {
-    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
