6fc3fdc1992649132ff5989e00838257263f5ff9
alanmartinc <alancabot.ac@gmail.com>
2021-06-06 20:00:32 -0300

    Se agrega ejercicios configurando REDUX, instalando dev tools y creando el store

--- Sección 14 - PROYECTO REDUX Listado de Productos/crudredux/src/App.js
+++ Sección 14 - PROYECTO REDUX Listado de Productos/crudredux/src/App.js
@@ -5,18 +5,24 @@ import NuevoProducto from './components/NuevoProducto';
 import EditarProducto from './components/EditarProducto';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
+// Redux
+import {Provider} from 'react-redux';
+import store from './store';
+
 function App() {
   return(
     <Router>
-      <Header/>
+      <Provider store={store}>
+        <Header/>
 
-      <div className="container mt-5">
-        <Switch>
-          <Route exact path="/" component={Productos}/>
-          <Route exact path="/productos/nuevo" component={NuevoProducto}/>
-          <Route exact path="/productos/editar/:id" component={EditarProducto}/>
-        </Switch>
-      </div>
+        <div className="container mt-5">
+          <Switch>
+            <Route exact path="/" component={Productos}/>
+            <Route exact path="/productos/nuevo" component={NuevoProducto}/>
+            <Route exact path="/productos/editar/:id" component={EditarProducto}/>
+          </Switch>
+        </div>
+      </Provider>
     </Router>
   );
 }
