d9116414f537015b9ebec6bea938472731255bb2
alanmartinc <alancabot.ac@gmail.com>
2021-06-14 12:59:45 -0300

    Se agrega ejercicios llevando al usuario a edición

--- Sección 14 - PROYECTO REDUX Listado de Productos/crudredux/src/components/Producto.js
+++ Sección 14 - PROYECTO REDUX Listado de Productos/crudredux/src/components/Producto.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import Swal from 'sweetalert2';
 
 // Redux
@@ -12,6 +12,7 @@ const Producto = ({producto}) => {
     const {nombre, precio, id} = producto;
 
     const dispatch = useDispatch();
+    const history = useHistory(); // Habilitar history para redirección
 
     // Confirmar si desea eliminar
     const confirmarEliminarProducto = id => {
@@ -33,14 +34,21 @@ const Producto = ({producto}) => {
         });
     }
 
+    // Función que redirige de forma programada
+    const redireccionarEdicion = producto => {
+        history.push(`/productos/editar/${producto.id}`)
+    }
+
     return(
         <tr>
             <td>{nombre}</td>
             <td><span className="font-weight-bold">$ {precio}</span></td>
             <td className="acciones">
-                <Link to={`/productos/editar/${id}`} className="btn btn-primary mr-2">
-                    Editar
-                </Link>
+                <button 
+                    type="button" 
+                    onClick={() => redireccionarEdicion(producto)}
+                    className="btn btn-primary mr-2"
+                >Editar</button>
                 <button
                     type="button"
                     className="btn btn-danger"
