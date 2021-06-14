6b464769e438f4475759140af0bb8dcd717d0543
alanmartinc <alancabot.ac@gmail.com>
2021-06-14 12:53:38 -0300

    Se agrega ejercicios agregando SweetAlert

--- Sección 14 - PROYECTO REDUX Listado de Productos/crudredux/src/actions/productoActions.js
+++ Sección 14 - PROYECTO REDUX Listado de Productos/crudredux/src/actions/productoActions.js
@@ -103,8 +103,16 @@ export function borrarProductoAction(id) {
         try {
             await clienteAxios.delete(`/productos/${id}`);
             dispatch(eliminarProductoExito());
+
+            // Si se elimina, mostrar alerta
+            Swal.fire(
+                'Eliminado',
+                'El producto se eliminó correctamente',
+                'success'
+            )
         } catch (error) {
-            
+            console.log(error);
+            dispatch(eliminarProductoError());
         }
     }
 }
--- Sección 14 - PROYECTO REDUX Listado de Productos/crudredux/src/components/Producto.js
+++ Sección 14 - PROYECTO REDUX Listado de Productos/crudredux/src/components/Producto.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 // Redux
 import {useDispatch} from 'react-redux';
@@ -15,9 +16,21 @@ const Producto = ({producto}) => {
     // Confirmar si desea eliminar
     const confirmarEliminarProducto = id => {
         // Preguntar al usuario
-
-        // Pasarlo al action
-        dispatch(borrarProductoAction(id));
+        Swal.fire({
+            title: '¿Estas seguro?',
+            text: "Un producto que se elimina no se puede recuperar",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, Eliminar!!',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Pasarlo al action
+                dispatch(borrarProductoAction(id));
+            }
+        });
     }
 
     return(
