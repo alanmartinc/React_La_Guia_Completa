9c39c7932679a0ad4704fad46f8306820eb728e5
alanmartinc <alancabot.ac@gmail.com>
2021-06-17 16:00:54 -0300

    Se agrega ejercicios Routing en Next.js

---
+++ Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/pages/nosotros.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout';
+
+const Nosotros = () => (
+  <div>
+    <Layout>
+      <h1>Nosotros</h1>
+    </Layout>
+  </div> 
+)
+
+export default Nosotros
