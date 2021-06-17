f004c00c41177cbfd213e2a6b79b3a4541d62552
alanmartinc <alancabot.ac@gmail.com>
2021-06-17 16:07:19 -0300

    Se agrega ejercicios agregando elementos al Header

---
+++ Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/components/layouts/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Buscar from '../ui/Buscar'
+
+const Header = () => {
+    return(
+        <header>
+            <div>
+                <div>
+                    <p>P</p>
+
+                    <Buscar/>
+
+                    {/* Buscador aqui */}
+
+                    {/* Nav aqui */}
+                </div>
+
+                <div>
+                    {/* Menú de administración*/}
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
