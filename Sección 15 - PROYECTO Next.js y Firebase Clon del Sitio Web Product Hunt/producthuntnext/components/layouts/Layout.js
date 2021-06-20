806885801f16f6afda7c43e9b73c0ec5d5dd1c8f
alanmartinc <alancabot.ac@gmail.com>
2021-06-19 23:58:05 -0300

    Se agrega ejercicios agregando fuentes de Google Fonts y más

--- Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/components/layouts/Layout.js
+++ Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/components/layouts/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import {Global, css} from '@emotion/react'
+import Head from 'next/head'
 
 const Layout = props => {
     return(
@@ -25,11 +26,21 @@ const Layout = props => {
                     body {
                         font-size: 1.6rem; 
                         line-height: 1.5;
+                        font-family: 'PT Sans', sans-serif;
                     }
 
                     h1, h2, h3 {
                         margin: 0 0 2rem 0;
                         line-height: 1.5;
+                    }
+
+                    h1, h2 {
+                        font-family: 'Roboto Slab', serif;
+                        font-weight: 700;
+                    }
+
+                    h3 {
+                        font-family: 'PT Sans', sans-serif;
                     }
 
                     ul {
@@ -43,6 +54,16 @@ const Layout = props => {
                     }
                 `}
             />
+
+            <Head>
+                <html lag="es"/>
+                <title>Product Hunt Firebase y Next.js</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap" rel="stylesheet"/>
+                <link href="/static/css/app.css" rel="stylesheet"/>
+            </Head>
+
             <Header/>
 
             <main>
