8db5e251299ca11690f5bac1801883188686cafe
alanmartinc <alancabot.ac@gmail.com>
2021-06-21 19:33:37 -0300

    Se agrega ejercicios las reglas de validación

--- Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/pages/crear-cuenta.js
+++ Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/pages/crear-cuenta.js
@@ -4,56 +4,74 @@ import Layout from '../components/layouts/Layout'
 import {Formulario, Campo, InputSubmit} from '../components/ui/Formulario'
 import styled from '@emotion/styled'
 
+// Validaciones
+import useValidacion from '../hooks/useValidacion'
+import validarCrearCuenta from '../validacion/validarCrearCuenta'
+
+const STATE_INICIAL = {
+  nombre: '',
+  email: '',
+  password: ''
+}
+
 const Centrar = styled.h1`
   text-align: center;
   margin-top: 5rem;
 `;
 
-const CrearCuenta = () => (
-  <div>
-    <Layout>
-      <>
-        <Centrar>Crear Cuenta</Centrar>
+const CrearCuenta = () => {
+  const {valores, errores, submitForm, handleSubmit, handleChange} = useValidacion(STATE_INICIAL, validarCrearCuenta, CrearCuenta);
 
-        <Formulario>
-          <Campo>
-            <label htmlFor="nombre">Nombre</label>
-            <input 
-              type="text"
-              id="nombre"
-              placeholder="Tu Nombre"
-              name="nombre"
+  function CrearCuenta() {
+    console.log('Creando cuenta...');
+  }
+
+  return (
+    <div>
+      <Layout>
+        <>
+          <Centrar>Crear Cuenta</Centrar>
+  
+          <Formulario>
+            <Campo>
+              <label htmlFor="nombre">Nombre</label>
+              <input 
+                type="text"
+                id="nombre"
+                placeholder="Tu Nombre"
+                name="nombre"
+              />
+            </Campo>
+  
+            <Campo>
+              <label htmlFor="email">Email</label>
+              <input 
+                type="email"
+                id="email"
+                placeholder="Tu Email"
+                name="email"
+              />
+            </Campo>
+  
+            <Campo>
+              <label htmlFor="password">Password</label>
+              <input 
+                type="password"
+                id="password"
+                placeholder="Tu Password"
+                name="password"
+              />
+            </Campo>
+  
+            <InputSubmit 
+              type="submit"
+              value="Crear Cuenta"
             />
-          </Campo>
-
-          <Campo>
-            <label htmlFor="email">Email</label>
-            <input 
-              type="email"
-              id="email"
-              placeholder="Tu Email"
-              name="email"
-            />
-          </Campo>
-
-          <Campo>
-            <label htmlFor="password">Password</label>
-            <input 
-              type="password"
-              id="password"
-              placeholder="Tu Password"
-              name="password"
-            />
-          </Campo>
-
-          <InputSubmit 
-            type="submit"
-            value="Crear Cuenta"
-          />
-        </Formulario>
-      </>
-    </Layout>
-  </div> 
-)
+          </Formulario>
+        </>
+      </Layout>
+    </div> 
+  )
+}
 
 export default CrearCuenta
---
+++ Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/validacion/validarCrearCuenta.js
@@ -0,0 +1,24 @@
+export default function validarCrearCuenta(valores) {
+    let errores = {};
+
+    // Validar el nombre del usuario
+    if(!valores.nombre) {
+        errores.nombre = "El Nombre es Obligatorio"
+    }
+
+    // Validar el email
+    if(!valores.email) {
+        errores.email = "El Email es Obligatorio"
+    } else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(valores.email)) {
+        errores.email = "Email no válido"
+    }
+
+    // Validar el password
+    if(!valores.password) {
+        errores.password = "El Password es Obligatorio"
+    } else if(valores.password.length < 6) {
+        errores.password = "El password debe ser de al menos 6 caracteres"
+    }
+
+    return errores;
+}
