fcc9ea51bdc68dd6a8fa441c59297de94ccd6db5
alanmartinc <alancabot.ac@gmail.com>
2021-06-21 02:30:15 -0300

    Se agrega ejercicios CSS al Header

--- Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/components/layouts/Header.js
+++ Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/components/layouts/Header.js
@@ -1,14 +1,42 @@
 import React from 'react'
 import Buscar from '../ui/Buscar'
-import Navegacion from './Navegacion';
-import Link from 'next/link';
+import Navegacion from './Navegacion'
+import Link from 'next/link'
+import styled from '@emotion/styled'
+import {css} from '@emotion/react'
+
+const Cabecera = styled.header`
+    border-bottom: 2px solid var(--gris3);
+    padding: 1rem 0;
+`;
+
+const ContenedorHeader = styled.div`
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+    @media (min-width:768px) {
+        display: flex;
+        justify-content: space-between;
+    }
+`;
+
+const Logo = styled.p`
+    color: var(--naranja);
+    font-size: 4rem;
+    line-height: 0;
+    font-weight: 700;
+    font-family: 'Roboto Slab', serif;
+    margin-right: 2rem;
+`;
 
 const Header = () => {
     return(
-        <header>
-            <div>
+        <Cabecera>
+            <ContenedorHeader>
                 <div>
-                    <p>P</p>
+                    <Link href="/">
+                        <Logo>P</Logo>
+                    </Link>
 
                     <Buscar/>
 
@@ -23,8 +51,8 @@ const Header = () => {
                     <Link href="/">Login</Link>
                     <Link href="/">Crear Cuenta</Link>
                 </div>
-            </div>
-        </header>
+            </ContenedorHeader>
+        </Cabecera>
     );
 }
 
--- Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/components/layouts/Layout.js
+++ Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/components/layouts/Layout.js
@@ -11,6 +11,7 @@ const Layout = props => {
                     :root {
                         --gris: #3d3d3d;
                         --gris2: #6F6F6F;
+                        --gris3: #e1e1e1;
                         --naranja: #DA552F;
                     }
 
