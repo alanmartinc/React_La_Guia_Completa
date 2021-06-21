deda669cc7c9fbce3e5fb27344ff17dc34d559f3
alanmartinc <alancabot.ac@gmail.com>
2021-06-21 20:51:11 -0300

    Se agrega ejercicios validando el form y mostrando mensajes

--- Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/hooks/useValidacion.js
+++ Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/hooks/useValidacion.js
@@ -33,12 +33,18 @@ const useValidacion = (stateInicial, validar, fn) => {
         guardarSubmitForm(true);
     }
 
+    // Cuando se realiza el evento de blur
+    const handleBlur = () => {
+        const erroresValidacion = validar(valores);
+        guardarErrores(erroresValidacion);
+    }
+
     return {
         valores,
         errores,
-        submitForm,
         handleSubmit,
-        handleChange
+        handleChange,
+        handleBlur
     }
 }
 
--- Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/pages/crear-cuenta.js
+++ Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/pages/crear-cuenta.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {css} from '@emotion/react'
 import Layout from '../components/layouts/Layout'
-import {Formulario, Campo, InputSubmit} from '../components/ui/Formulario'
+import {Formulario, Campo, InputSubmit, Error} from '../components/ui/Formulario'
 import styled from '@emotion/styled'
 
 // Validaciones
@@ -20,7 +20,7 @@ const Centrar = styled.h1`
 `;
 
 const CrearCuenta = () => {
-  const {valores, errores, submitForm, handleSubmit, handleChange} = useValidacion(STATE_INICIAL, validarCrearCuenta, CrearCuenta);
+  const {valores, errores, handleSubmit, handleChange, handleBlur} = useValidacion(STATE_INICIAL, validarCrearCuenta, CrearCuenta);
 
   const {nombre, email, password} = valores;
 
@@ -47,8 +47,11 @@ const CrearCuenta = () => {
                 name="nombre"
                 value={nombre}
                 onChange={handleChange}
+                onBlur={handleBlur}
               />
             </Campo>
+
+            {errores.nombre && <Error>{errores.nombre}</Error>}
   
             <Campo>
               <label htmlFor="email">Email</label>
@@ -59,8 +62,11 @@ const CrearCuenta = () => {
                 name="email"
                 value={email}
                 onChange={handleChange}
+                onBlur={handleBlur}
               />
             </Campo>
+
+            {errores.email && <Error>{errores.email}</Error>}
   
             <Campo>
               <label htmlFor="password">Password</label>
@@ -71,8 +77,11 @@ const CrearCuenta = () => {
                 name="password"
                 value={password}
                 onChange={handleChange}
+                onBlur={handleBlur}
               />
             </Campo>
+
+            {errores.password && <Error>{errores.password}</Error>}
   
             <InputSubmit 
               type="submit"
