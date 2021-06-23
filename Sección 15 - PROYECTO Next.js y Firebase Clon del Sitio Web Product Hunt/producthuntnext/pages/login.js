b416df040534910c79257f059f3821d5b798c9f1
alanmartinc <alancabot.ac@gmail.com>
2021-06-23 14:07:55 -0300

    Se agrega ejercicios iniciando sesión de usuarios

--- Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/pages/login.js
+++ Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/pages/login.js
@@ -27,8 +27,14 @@ const Login = () => {
 
   const {email, password} = valores;
 
-  function iniciarSesion() {
-    console.log('Iniciando Sesión...');
+  async function iniciarSesion() {
+    try {
+      await firebase.login(email, password);
+      Router.push('/');
+    } catch (error) {
+      console.log('Hubo un error al autenticar el usuario ', error.message);
+      guardarError(error.message);
+    }
   }
 
   return (
