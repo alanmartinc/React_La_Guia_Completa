08d925522a3e2a111bd28710cc5f11100cf8586c
alanmartinc <alancabot.ac@gmail.com>
2021-07-02 17:38:00 -0300

    Se agrega ejercicios mostrando información de la base de datos

--- Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/components/layouts/DetallesProductos.js
+++ Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/components/layouts/DetallesProductos.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import styled from '@emotion/styled'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import {es} from 'date-fns/locale'
+
+const Imagen = styled.img`
+    width: 200px;
+`;
 
 const DetallesProductos = ({producto}) => {
     const {id, comentarios, creado, descripcion, empresa, nombre, url, urlimagen, votos} = producto;
@@ -7,16 +14,26 @@ const DetallesProductos = ({producto}) => {
         <li>
             <div>
                 <div>
-
+                    <Imagen src={urlimagen}/>
                 </div>
 
                 <div>
                     <h1>{nombre}</h1>
+
+                    <p>{descripcion}</p>
+
+                    <div>
+                        <img src="/static/img/comentario.png"/>
+                        <p>{comentarios.length} Comentarios</p>
+                    </div>
+
+                    <p>Publicado hace: {formatDistanceToNow(new Date(creado), {locale: es})}</p>
                 </div>
             </div>
 
             <div>
-
+                <div> &#9650; </div>
+                <p>{votos}</p>
             </div>
         </li>
     );
