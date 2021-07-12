8b9c8ed82754bfb6e66bad8157014710f8d0002c
alanmartinc <alancabot.ac@gmail.com>
2021-07-12 15:16:43 -0300

    Cuando se crea el proyecto ingresar configuración SDK al archivo config.js

--- Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/firebase/config.js
+++ Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/firebase/config.js
@@ -1,10 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBIhJcW6_YJ0O3xtmvDk2OPTbWkd5JZxPA",
-    authDomain: "project-product-hunt-b0ab0.firebaseapp.com",
-    projectId: "project-product-hunt-b0ab0",
-    storageBucket: "project-product-hunt-b0ab0.appspot.com",
-    messagingSenderId: "365536866376",
-    appId: "1:365536866376:web:85c14b3b8d90251886a87d"
+    // Ingresar aquí su configuración SDK al crear el proyecto
 };
 
 export default firebaseConfig;
