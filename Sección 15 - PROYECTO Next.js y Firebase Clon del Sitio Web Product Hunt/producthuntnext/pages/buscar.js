952eec7e3676af06e841b51684b7ad3ffa74dfba
alanmartinc <alancabot.ac@gmail.com>
2021-07-12 00:40:49 -0300

    Se agrega ejercicios filtrando los productos

--- Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/pages/buscar.js
+++ Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/pages/buscar.js
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Layout from '../components/layouts/Layout'
-import { useRouter } from 'next/router'
+import {useRouter} from 'next/router'
+import DetallesProductos from '../components/layouts/DetallesProductos'
+import useProductos from '../hooks/useProductos'
 
 const Buscar = () => {
   const router = useRouter();
   const {query: {q}} = router;
-  console.log(q);
+
+  // Todos los productos
+  const {productos} = useProductos('creado');
+  const [resultado, guardarResultado] = useState([]);
+
+  useEffect(() => {
+      const busqueda = q.toLowerCase()
+      const filtro = productos.filter(producto => {
+        return (
+          producto.nombre.toLowerCase().includes(busqueda) || 
+          producto.descripcion.toLowerCase().includes(busqueda)
+        )
+      });
+      guardarResultado(filtro);
+  }, [q, productos]);
 
   return (
     <div>
       <Layout>
-        <h1>Buscar</h1>
+        <div className="listado-productos">
+          <div className="contenedor">
+            <ul className="bg-white">
+              {resultado.map(producto => (
+                <DetallesProductos
+                  key={producto.id}
+                  producto={producto}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
       </Layout>
-    </div> 
+    </div>
   )
 }
 
