2e99fe47f478d30680a359b13c3310f113a5e2df
alanmartinc <alancabot.ac@gmail.com>
2021-07-11 23:52:42 -0300

    Se agrega ejercicios pasando la búsqueda al componente

--- Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/pages/buscar.js
+++ Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/pages/buscar.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import Layout from '../components/layouts/Layout';
+import Layout from '../components/layouts/Layout'
+import { useRouter } from 'next/router'
 
-const Buscar = () => (
-  <div>
-    <Layout>
-      <h1>Buscar</h1>
-    </Layout>
-  </div> 
-)
+const Buscar = () => {
+  const router = useRouter();
+  const {query: {q}} = router;
+  console.log(q);
+
+  return (
+    <div>
+      <Layout>
+        <h1>Buscar</h1>
+      </Layout>
+    </div> 
+  )
+}
 
 export default Buscar
