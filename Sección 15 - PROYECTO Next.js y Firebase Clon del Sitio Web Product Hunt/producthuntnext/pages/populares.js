8ca97a327f302ae4b6d77fb3c9c87977fb75453a
alanmartinc <alancabot.ac@gmail.com>
2021-07-11 23:34:23 -0300

    Se agrega ejercicios mostrando productos por creado y por votos

--- Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/pages/populares.js
+++ Sección 15 - PROYECTO Next.js y Firebase Clon del Sitio Web Product Hunt/producthuntnext/pages/populares.js
@@ -1,12 +1,29 @@
 import React from 'react'
-import Layout from '../components/layouts/Layout';
+import Layout from '../components/layouts/Layout'
+import DetallesProductos from '../components/layouts/DetallesProductos'
+import useProductos from '../hooks/useProductos'
 
-const Populares = () => (
-  <div>
-    <Layout>
-      <h1>Populares</h1>
-    </Layout>
-  </div> 
-)
+const Populares = () => {
+  const {productos} = useProductos('votos');
+
+  return (
+    <div>
+      <Layout>
+        <div className="listado-productos">
+          <div className="contenedor">
+            <ul className="bg-white">
+              {productos.map(producto => (
+                <DetallesProductos
+                  key={producto.id}
+                  producto={producto}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </Layout>
+    </div> 
+  )
+}
 
 export default Populares
